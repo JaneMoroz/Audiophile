e924453ecdd23c47fb48225c1efc06558267786b
Jane Moroz <Moroz.jane@yahoo.com>
2023-01-20 18:37:24 +0300

    feat: add colors, typography and button styles

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -3,7 +3,28 @@ import tw, { GlobalStyles as BaseStyles } from 'twin.macro'
 
 const CustomStyles = createGlobalStyle({
   body: {
-    ...tw`bg-amber-50`,
+    ...tw`bg-background font-body`,
+  },
+  h1: {
+    ...tw`text-[56px] font-bold leading-[56px] tracking-[2px] uppercase`,
+  },
+  h2: {
+    ...tw`text-[40px] font-bold leading-[44px] tracking-[1.5px] uppercase`,
+  },
+  h3: {
+    ...tw`text-[32px] font-bold leading-[36px] tracking-[1.15px]  uppercase`,
+  },
+  h4: {
+    ...tw`text-[28px] font-bold leading-[38px] tracking-[2px]  uppercase`,
+  },
+  h5: {
+    ...tw`text-[24px] font-bold leading-[33px] tracking-[1.7px]  uppercase`,
+  },
+  h6: {
+    ...tw`text-[18px] font-bold leading-[24px] tracking-[1.3px]  uppercase`,
+  },
+  p: {
+    ...tw`text-[15px] font-normal leading-[25px]`,
   },
 })
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#fbaf85',
+          200: '#D87D4A',
+        },
+        secondary: '#101010',
+        tertiary: '#F1F1F1',
+        background: '#FAFAFA',
+      },
+      fontFamily: {
+        body: ['Manrope'],
+      },
+      boxShadow: {
+        button: 'inset 0 0 0 1px black',
+      },
+    },
   },
   plugins: [],
 }
